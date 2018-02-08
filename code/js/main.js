ab238122a169017c705c9dcf2d25cc211701a64e
Sagar <sagy26.1991@gmail.com>
2018-02-08 18:24:36 +0530

    add javascript

--- code/js/main.js
+++ code/js/main.js
@@ -6,6 +6,26 @@ $(document).ready(function(){
 	})
 
 	
+	$("#pickup-next").on("click", function(e){
+		$("a[href='#drop']").tab("show");
+	});	
+
+	$("#drop-previous").on("click", function(e){
+		$("a[href='#pickup']").tab("show");
+	});	
+
+	$("#drop-next").on("click", function(e){
+		$("a[href='#profile']").tab("show");
+	});
+
+	$("#profile-previous").on("click", function(e){
+		$("a[href='#drop']").tab("show");
+	});
+
+	$("#profile-next").on("click", function(e){
+		// $("a[href='#drop']").tab("show");
+	});
+
 });
 
 
