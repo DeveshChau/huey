1f37ec103ec9979c96b5fb26266d74ebd107bf1b
Sagar <sagy26.1991@gmail.com>
2018-02-12 19:33:50 +0530

    home page change

--- code/index.php
+++ code/index.php
@@ -9,8 +9,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" href="./css/style.css">
 		<style type="text/css">
-			html { 
-			  	background: url(img/bg.png) no-repeat center center fixed; 
+			html {
+			  	background: url(img/bg.png) no-repeat center center fixed;
 			  	-webkit-background-size: cover;
 			  	-moz-background-size: cover;
 			  	-o-background-size: cover;
@@ -21,8 +21,10 @@
   	<body>
   		<?php include 'header.php' ?>
   		<main role="main">
-  			<p>Stress free moving just a call away</p>
-      		<a href="details.php" class="btn btn-primary">Move with pacemove</a>
+        <div id="order-wrapper">
+    			<p>Stress free moving just a call away</p>
+        	<a href="details.php" class="btn btn-primary">Move with pacemove</a>
+        </div>
   		</main>
 		<script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
 		<script type="text/javascript" src="http://ajax.cdnjs.com/ajax/libs/json2/20110223/json2.js"></script>
